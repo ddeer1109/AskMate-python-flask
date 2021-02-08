9f76bace31cd6d49f3db40c610b22120c8b215d5
ddeer1109 <dmwator11@gmail.com>
2021-02-08 15:10:15 +0100

    Displaying question with answers

--- data_handler.py
+++ data_handler.py
@@ -5,18 +5,23 @@ import pathlib
 from datetime import datetime
 
 
-DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else f"{pathlib.Path(__file__).parent.absolute()}/sample_data/question.csv"
-DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+QUESTIONS_DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else f"{pathlib.Path(__file__).parent.absolute()}/sample_data/question.csv"
+ANSWER_DATA_FILE_PATH = os.getenv('DATA_FILE_PATH') if 'DATA_FILE_PATH' in os.environ else f"{pathlib.Path(__file__).parent.absolute()}/sample_data/answer.csv"
+QUESTIONS_DATA_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image']
+ANSWERS_DATA_HEADER = ['id','submission_time','vote_number','message','image']
 
 
-def get_formatted_headers():
-    return [header.replace("_", " ").capitalize() for header in DATA_HEADER]
+def get_formatted_headers(datatype="question"):
+    if datatype == "question":
+        return [header.replace("_", " ").capitalize() for header in QUESTIONS_DATA_HEADER]
+    elif datatype == "answer":
+        return [header.replace("_", " ").capitalize() for header in ANSWERS_DATA_HEADER]
 
 
 def get_all_questions():
     questions = []
 
-    with open(DATA_FILE_PATH) as csv_file:
+    with open(QUESTIONS_DATA_FILE_PATH) as csv_file:
         csv_reader = csv.DictReader(csv_file, delimiter=',', quotechar='"')
         for row in csv_reader:
             questions.append(row)
@@ -24,23 +29,48 @@ def get_all_questions():
         return questions
 
 
-def sort_questions_by_time():
-    questions = get_all_questions()
-    new_list = sorted(questions, key=lambda k: k['submission_time'])
+def get_all_answers():
+    answers = []
 
-    for question in new_list:
-        time_stamp = int(question["submission_time"])
+    with open(ANSWER_DATA_FILE_PATH) as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',', quotechar='"')
+        for row in csv_reader:
+            answers.append(row)
+
+        return answers
+
+
+def sort_data_by_time(entries_list):
+    new_list = sorted(entries_list, key=lambda k: k['submission_time'])
+
+    for entry in new_list:
+        time_stamp = int(entry["submission_time"])
         dt_object = datetime.fromtimestamp(time_stamp)
-        question["submission_time"] = dt_object
+        entry["submission_time"] = dt_object
 
     return new_list
 
 
 def get_question_by_id(id):
-    questions = sort_questions_by_time()
+    questions_unformatted = get_all_questions()
+    questions = sort_data_by_time(questions_unformatted)
 
     for question in questions:
         if question["id"] == id:
             return question
+
+
+def get_answers_for_question(question_id):
+    answers = sort_data_by_time(get_all_answers())
+
+    question_answers = []
+
+    for answer in answers:
+        if answer["id"] == question_id:
+            question_answers.append(answer)
+
+    return question_answers
+
+
 
 
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-from data_handler import get_all_questions, sort_questions_by_time, get_question_by_id, get_formatted_headers
+from data_handler import get_all_questions, sort_data_by_time, get_question_by_id, get_formatted_headers, get_answers_for_question
 
 app = Flask(__name__)
 
@@ -11,17 +11,19 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/list")
 def get_list_of_questions():
-    # questions = get_all_questions()
-    headers = get_formatted_headers()
-    questions = sort_questions_by_time()
-    return render_template('list.html', questions=questions, headers=headers)
+    questions = get_all_questions()
+    question_headers = get_formatted_headers(datatype="question")
+    questions = sort_data_by_time(questions)
+    return render_template('list.html', questions=questions, question_headers=question_headers)
 
 
 @app.route("/question/<question_id>")
 def display_question(question_id):
-    headers = get_formatted_headers()
+    question_headers = get_formatted_headers(datatype="question")
+    answer_headers = get_formatted_headers(datatype="answer")
     question = get_question_by_id(question_id)
-    return render_template("question.html", question=question, headers=headers)
+    answers = get_answers_for_question(question_id)
+    return render_template("question.html", question=question, answers=answers, question_headers=question_headers, answer_headers=answer_headers)
 
 
 if __name__ == "__main__":
