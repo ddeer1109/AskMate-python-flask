52b5e43e368c816183cbce5f73a179c8d0f69ecd
Daniel Wątor [KRK NOV 2020] <dmwator11@gmail.com>
2021-02-20 11:22:47 +0100

    Notes what to update before 2nd sprint

--- data_manager.py
+++ data_manager.py
@@ -6,10 +6,14 @@ import os
 
 
 def get_formatted_headers(headers):
+    """Gets human-display formatted headers (for table headers)."""
+
     return [header.replace("_", " ").capitalize() for header in headers]
 
 
 def sort_data(questions, requested_query_string, header):
+    """Returns data (for this feature implemented for questions) sorted by requested_query_string"""
+
     sorting_key = {
         'order_by': 'submission_time',
         'order_direction': 'desc'
@@ -24,6 +28,8 @@ def sort_data(questions, requested_query_string, header):
 
 
 def sort_data_by_sorting_key(entries_list, sorting_key):
+    """Engine of sorting"""
+
     if sorting_key['order_by'] in ['view_number', 'vote_number']:
         # it is necessary to cast value to int to use function sort
         for entry in entries_list:
@@ -38,6 +44,8 @@ def sort_data_by_sorting_key(entries_list, sorting_key):
 
 
 def convert_timestamp(entry):
+    """Returns entry with human-friendly converted timestamp"""
+
     time_stamp = int(entry["submission_time"])
     converted_time = datetime.fromtimestamp(time_stamp)
     entry["submission_time"] = converted_time
@@ -46,24 +54,32 @@ def convert_timestamp(entry):
 
 
 def get_question_by_id(question_id, questions):
+    """Returns question (entry) of given id from list with converted timestamp"""
+
     for question in questions:
         if question["id"] == question_id:
             return convert_timestamp(question)
 
 
 def get_question_by_id_without_timestamp_conversion(question_id, questions):
+    """As above but without timestamp convertion"""
+
     for question in questions:
         if question["id"] == question_id:
             return question
 
 
 def get_entry_by_id(entry_id, entries_list):
+    """As above but without timestamp convertion"""
+
     for entry in entries_list:
         if entry['id'] == entry_id:
             return entry
 
 
 def get_answers_for_question(question_id, answers):
+    """Returns list of answer entries for question of given id"""
+
     question_answers = []
     for answer in answers:
         if answer["question_id"] == question_id:
@@ -73,6 +89,8 @@ def get_answers_for_question(question_id, answers):
 
 
 def filter_data(dict_data, headers):
+    """Returns entry filtered by wished headers"""
+
     filtered_data = []
     try:
         for entry in dict_data:
@@ -88,6 +106,8 @@ def filter_data(dict_data, headers):
 
 
 def get_next_id(entries_list):
+    """Computes next id based on given entries list."""
+
     next_id = 0
     for entry in entries_list:
         entry_id = int(entry['id'])
@@ -98,6 +118,9 @@ def get_next_id(entries_list):
 
 
 def delete_rows(question_id, criteria, entries_list):
+    """Returns new list without deleted id entry. Services deletion of question and associated answers
+    and their images."""
+
     temp_list = []
 
     for entry in entries_list:
@@ -115,16 +138,22 @@ def delete_rows(question_id, criteria, entries_list):
 
 
 def get_current_timestamp():
+    """Return current timestamp in seconds"""
+
     return int(time.time())
 
 
 def set_initial_values(entry_dictionary, listed_base_data):
+    """Set initial values for entry_dictionary based on database data (listed_base_data)"""
+
     entry_dictionary['submission_time'] = str(get_current_timestamp())
     entry_dictionary['vote_number'] = '0'
     entry_dictionary['id'] = str(get_next_id(listed_base_data))
 
 
 def add_new_question(form_data, request_files):
+    """Engine of adding new question."""
+
     questions = data_handler.read_file(data_handler.QUESTIONS_DATA_FILE_PATH)
     requested_data = dict(form_data)
 
@@ -144,6 +173,8 @@ def add_new_question(form_data, request_files):
 
 
 def add_new_answer(form_data, request_files, question_id):
+    """Engine of adding new answer."""
+
     answers = data_handler.read_file(data_handler.ANSWER_DATA_FILE_PATH)
     requested_data = dict(form_data)
 
@@ -163,6 +194,7 @@ def add_new_answer(form_data, request_files, question_id):
 
 def process_image_input(image_storage_obj, sub_dir, entry_id):
     """Checks if storage object is not empty. If it is returns default image filename else returns object filename"""
+
     storage_obj_empty = image_storage_obj.filename == ""
     invalid_extension = ".jpg" not in image_storage_obj.filename and ".png" not in image_storage_obj.filename
 
@@ -176,12 +208,16 @@ def process_image_input(image_storage_obj, sub_dir, entry_id):
 
 
 def update_entry(updated_entry, entries_list):
+    """Updates entry in entries_list"""
+
     for entry in entries_list:
         if entry['id'] == updated_entry['id']:
             entries_list[entries_list.index(entry)] = updated_entry
 
 
 def vote_on_post(entry_id, vote_value, entry_type):
+    """Engine for voting on any type of post on website."""
+
     if entry_type == "question":
         entries_list = data_handler.read_file(data_handler.QUESTIONS_DATA_FILE_PATH)
     else:
@@ -207,6 +243,8 @@ def vote_on_post(entry_id, vote_value, entry_type):
 
 
 def edit_entry(form_data, entry, entries_list):
+    """Engine for editing any type on post on website."""
+
     entry['message'] = form_data['message']
     entry['vote_number'] = '0'
 
@@ -223,6 +261,8 @@ def edit_entry(form_data, entry, entries_list):
 
 
 def delete_answer(answer_id):
+    """Deletes answer of given id from database."""
+
     answers = data_handler.read_file(data_handler.ANSWER_DATA_FILE_PATH)
     answer = get_entry_by_id(answer_id, answers)
     redirection_id = answer['question_id']
